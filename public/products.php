2547ff71865da0414ba750e0b28a4da19addab3f
Benjamin-51 <babilliot.benjamin@gmail.com>
2021-10-21 11:16:32 +0200

    Commit for the second quest

--- public/products.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../config/twig.php';
-$products = ['guitare', 'bass', 'banjo', 'cithare', 'lyre', 'harpe', 'piano'];
-
-
-echo $twig->render('products.html.twig', ['products' => $products]);
